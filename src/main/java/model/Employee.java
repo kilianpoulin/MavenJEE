55d1a1d495b75d2e64f1e48f69971ab1f5763a09
kilianpoulin <kilian.poulin@efrei.net>
2018-12-18 08:42:06 +0100

    Initial commit

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,100 @@
+package model;
+
+/**
+ *
+ * @author Pas mathieu c'est sûr
+ */
+public class Employee {
+    private String _Name;
+    private String _FirstName;
+    private String _HomePhone;
+    private String _MobilePhone;
+    private String _WorkPhone;
+    private String _Address;
+    private String _PostalCode;
+    private String _City;
+    private String _Email;
+    private String _ID;
+
+    public String getName() {
+        return _Name;
+    }
+
+    public String getFirstName() {
+        return _FirstName;
+    }
+
+    public String getHomePhone() {
+        return _HomePhone;
+    }
+
+    public String getMobilePhone() {
+        return _MobilePhone;
+    }
+
+    public String getWorkPhone() {
+        return _WorkPhone;
+    }
+
+    public String getAddress() {
+        return _Address;
+    }
+
+    public String getPostalCode() {
+        return _PostalCode;
+    }
+
+    public String getCity() {
+        return _City;
+    }
+
+    public String getEmail() {
+        return _Email;
+    }
+
+    public String getID() {
+        return _ID;
+    }
+
+    public void setName(String _Name) {
+        this._Name = _Name;
+    }
+
+    public void setFirstName(String _FirstName) {
+        this._FirstName = _FirstName;
+    }
+
+    public void setHomePhone(String _HomePhone) {
+        this._HomePhone = _HomePhone;
+    }
+
+    public void setMobilePhone(String _MobilePhone) {
+        this._MobilePhone = _MobilePhone;
+    }
+
+    public void setWorkPhone(String _WorkPhone) {
+        this._WorkPhone = _WorkPhone;
+    }
+
+    public void setAddress(String _Address) {
+        this._Address = _Address;
+    }
+
+    public void setPostalCode(String _PostalCode) {
+        this._PostalCode = _PostalCode;
+    }
+
+    public void setCity(String _City) {
+        this._City = _City;
+    }
+
+    public void setEmail(String _Email) {
+        this._Email = _Email;
+    }
+
+    public void setID(String _ID) {
+        this._ID = _ID;
+    }
+    
+
+}
